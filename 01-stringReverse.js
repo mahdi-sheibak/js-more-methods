3c93a3b50e8ddac3612527d3a90a7f7663219185
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2021-12-11 06:12:03 +0330

    Feat: 01-stringReverse

---
+++ 01-stringReverse.js
@@ -0,0 +1,6 @@
+const stringReverse = (str = "") => {
+  return str.split("").reverse().join("");
+};
+
+console.log(stringReverse("Hello World!")); // !dlroW olleH
+console.log(stringReverse("Mahdi zoraghi")); // ihgaroz idhaM
