cdc9b4e1e5d95025bab56311b138634af2f0ca36
Mahdi zoraghi <mahdi.zoraghi.sheibak@gmail.com>
2021-12-19 07:31:05 +0330

    Feat: 21-titleCase

---
+++ 21-titleCase.js
@@ -0,0 +1,18 @@
+// const titleCase = (str = "") => {
+//   return str.split(" ").reduce((prevItem, currentItem) => {
+//     const word = currentItem[0].toUpperCase() + currentItem.slice(1);
+//     return prevItem + word + " ";
+//   }, "");
+// };
+
+const titleCase = (str = "") => {
+  const words = [];
+  for (const word of str.split(" "))
+    words.push(word[0].toUpperCase() + word.slice(1));
+
+  return words.join(" ");
+};
+
+console.log(titleCase("a short sentence")); // A Short Sentence
+console.log(titleCase("a lazy fox")); // A Lazy Fox
+console.log(titleCase("look, it is working!")); // Look, It Is Working!
